58c760a87cd39ab72e47bef8d1b34599334adcf5
yanxihang <964343812@qq.com>
2023-09-07 13:12:18 +0800

    Initialize Project again

---
+++ Source/GAS_RPG_TopView/GAS_RPG_TopView.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAS_RPG_TopView.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAS_RPG_TopView, "GAS_RPG_TopView" );
---
+++ Source/GAS_RPG_TopView/GAS_RPG_TopViewGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GAS_RPG_TopViewGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_RPG_TOPVIEW_API AGAS_RPG_TopViewGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

044fd1a0d5056c8601dda218421f6228b13a77ce
yanxihang <964343812@qq.com>
2023-09-28 00:44:29 +0800

    Effect actor revisited

--- Source/GAS_RPG_TopView/Public/Actor/AuraEffectActor.h
+++ Source/GAS_RPG_TopView/Public/Actor/AuraEffectActor.h
@@ -6,7 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "AuraEffectActor.generated.h"
 
-class USphereComponent;
+
+class UGameplayEffect;
 
 UCLASS()
 class GAS_RPG_TOPVIEW_API AAuraEffectActor : public AActor
@@ -16,17 +17,13 @@ class GAS_RPG_TOPVIEW_API AAuraEffectActor : public AActor
 public:	
 	AAuraEffectActor();
 
-	UFUNCTION()
-	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
-	UFUNCTION()
-	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 protected:
 	virtual void BeginPlay() override;
 private:
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> Sphere;
+	UFUNCTION(BlueprintCallable)
+	void ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass);
 
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<UStaticMeshComponent> Mesh;
+	UPROPERTY(EditAnywhere, Category = "Apply Effects")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 };

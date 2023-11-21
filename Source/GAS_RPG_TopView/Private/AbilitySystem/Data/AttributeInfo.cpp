4eb46fb8a53b21d032eb240ed62469cce593b2c9
yanxihang <964343812@qq.com>
2023-11-21 22:01:46 +0800

    Attribute Info Data Asset

---
+++ Source/GAS_RPG_TopView/Private/AbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Data/AttributeInfo.h"
+
+FAuraAttributeInfo UAttributeInfo::FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound) const
+{
+	for(const FAuraAttributeInfo& info:AttributeInformation)
+	{
+		if(info.AttributeTag.MatchesTagExact(AttributeTag))
+		{
+			return info;
+		}
+	}
+
+	if(bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find info for AttributeTag [%s] on AttributeInfo [%s]."), *AttributeTag.ToString(), *GetNameSafe(this));
+	}
+	return FAuraAttributeInfo();
+}

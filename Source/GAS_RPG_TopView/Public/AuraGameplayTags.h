9753f64e46596fbe318f43752239d1ab9ca84fb8
yanxihang <964343812@qq.com>
2023-11-21 09:39:58 +0800

    GAS_RPG_Top_View Asset Manager

--- Source/GAS_RPG_TopView/Public/AuraGameplayTags.h
+++ Source/GAS_RPG_TopView/Public/AuraGameplayTags.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 
 
 /**
@@ -15,6 +16,8 @@ struct FAuraGameplayTags
 public:
  static const FAuraGameplayTags Get() {return GameplayTags;}
  static void InitializeNativeGameplayTags();
+
+ FGameplayTag Attributes_Secondary_Armor;
  
 protected:
  

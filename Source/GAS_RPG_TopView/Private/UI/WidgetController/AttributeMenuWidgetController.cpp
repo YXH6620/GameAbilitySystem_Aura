b493caf000a65f519f982dabe9bcfee46c7aaac6
yanxihang <964343812@qq.com>
2023-11-23 13:45:26 +0800

    Responding to Attribute Changes

--- Source/GAS_RPG_TopView/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/GAS_RPG_TopView/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -3,25 +3,39 @@
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
 
-#include "AuraGameplayTags.h"
 #include "AbilitySystem/AuraAttributeSet.h"
 #include "AbilitySystem/Data/AttributeInfo.h"
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
-	
+	UAuraAttributeSet* AS =CastChecked<UAuraAttributeSet>(AttributeSet);
+	check(AttributeInfo);
+	for(auto &Pair: AS->TagsToAttributes)
+	{
+		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Pair.Value()).AddLambda(
+		[this, Pair](const FOnAttributeChangeData& Data)
+		{
+			BroadcastAttributeInfo(Pair.Key, Pair.Value());
+		}
+		);
+	}
 }
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
 	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
-
 	check(AttributeInfo);
 
 	for(auto &Pair: AS->TagsToAttributes)
 	{
-		FAuraAttributeInfo info = AttributeInfo->FindAttributeInfoForTag(Pair.Key);
-		info.AttributeValue = Pair.Value().GetNumericValue(AS);
-		AttributeInfoDelegate.Broadcast(info);
+		BroadcastAttributeInfo(Pair.Key, Pair.Value());
 	}
+}
+
+void UAttributeMenuWidgetController::BroadcastAttributeInfo(const FGameplayTag& AttributeTag,
+	const FGameplayAttribute& Attribute) const
+{
+	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(AttributeTag);
+	Info.AttributeValue = Attribute.GetNumericValue(AttributeSet);
+	AttributeInfoDelegate.Broadcast(Info);
 }

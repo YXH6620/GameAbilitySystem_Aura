a1986cb96cbb1ce19ee71e9d16d93af493ed3dc5
yanxihang <964343812@qq.com>
2023-11-26 14:18:35 +0800

    Callbacks for Ability Input

--- Source/GAS_RPG_TopView/Public/Player/AuraPlayerController.h
+++ Source/GAS_RPG_TopView/Public/Player/AuraPlayerController.h
@@ -3,11 +3,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
 
+class UAuraInputConfig;
 class UInputMappingContext;
 class UInputAction;
 struct FInputActionValue;
@@ -41,4 +43,11 @@ private:
 	void CursorTrace();
 	IEnemyInterface* LastActor;
 	IEnemyInterface* ThisActor;
+
+	void AbilityInputTagPressed(FGameplayTag InputTag);
+	void AbilityInputTagReleased(FGameplayTag InputTag);
+	void AbilityInputTagHeld(FGameplayTag InputTag);
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UAuraInputConfig> InputConfig;
 };

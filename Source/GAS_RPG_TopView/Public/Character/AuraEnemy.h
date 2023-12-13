933e79cadcbf4df5d36d6df3da776a583d0160df
yanxihang <964343812@qq.com>
2023-12-13 13:53:30 +0800

    Enemy Health Bar

--- Source/GAS_RPG_TopView/Public/Character/AuraEnemy.h
+++ Source/GAS_RPG_TopView/Public/Character/AuraEnemy.h
@@ -5,8 +5,10 @@
 #include "CoreMinimal.h"
 #include "Character/AuraCharacterBase.h"
 #include "Interaction/EnemyInterface.h"
+#include "UI/WidgetController/OverlayWidgetController.h"
 #include "AuraEnemy.generated.h"
 
+class UWidgetComponent;
 /**
  * 
  */
@@ -27,6 +29,12 @@ public:
 	virtual int32 GetPlayerLevel() override;
 	/** End Combat Interface **/
 
+	UPROPERTY(BlueprintAssignable)
+	FOnAtrributeChangedSignature OnHealthChanged;
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnAtrributeChangedSignature OnMaxHealthChanged;
+	
 protected:
 	virtual void BeginPlay() override;
 
@@ -34,4 +42,7 @@ protected:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Class Defaults")
 	int32 Level = 1;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UWidgetComponent> HealthBar;
 };

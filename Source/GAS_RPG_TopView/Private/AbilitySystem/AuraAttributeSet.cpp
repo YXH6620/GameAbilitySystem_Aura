7d3e189212a59ce78da17f930e5aad6c72bf36d0
yanxihang <964343812@qq.com>
2023-12-18 00:04:02 +0800

    Enemy Death

--- Source/GAS_RPG_TopView/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/GAS_RPG_TopView/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -7,6 +7,7 @@
 #include "AuraGameplayTags.h"
 #include "GameFramework/Character.h"
 #include "GameplayEffectExtension.h"
+#include "Interaction/CombatInterface.h"
 
 #include "Net/UnrealNetwork.h"
 
@@ -103,7 +104,15 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
 			SetHealth(FMath::Clamp(newHealth, 0.f, GetMaxHealth()));
 			
 			const bool bFatal = newHealth <= 0.f;
-			if(!bFatal)
+			if(bFatal)
+			{
+				ICombatInterface* CombatInterface = Cast<ICombatInterface>(Props.TargetAvatarActor);
+				if(CombatInterface)
+				{
+					CombatInterface->Die();
+				}
+			}
+			else
 			{
 				FGameplayTagContainer TagContainer;
 				TagContainer.AddTag(FAuraGameplayTags::Get().Effects_HitReact);

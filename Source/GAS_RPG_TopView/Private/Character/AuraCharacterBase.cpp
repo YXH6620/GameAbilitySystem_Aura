e3ae8a5e0e4f5134b1da0b7bad31a22b7460375a
yanxihang <964343812@qq.com>
2023-12-18 00:46:34 +0800

    Dissolve Effect

--- Source/GAS_RPG_TopView/Private/Character/AuraCharacterBase.cpp
+++ Source/GAS_RPG_TopView/Private/Character/AuraCharacterBase.cpp
@@ -49,6 +49,7 @@ void AAuraCharacterBase::MulticastHandleDeath_Implementation()
 	GetMesh()->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Block);
 
 	GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	Dissolve();
 }
 
 // Called when the game starts or when spawned
@@ -92,6 +93,22 @@ void AAuraCharacterBase::AddCharacterAbilities()
 	if(!HasAuthority()) return ;
 
 	AuraASC->AddCharacterAbilities(StartupAbility);
+}
+
+void AAuraCharacterBase::Dissolve()
+{
+	if (IsValid(DissolveMaterialInstance))
+	{
+		UMaterialInstanceDynamic* DynamicMatInst = UMaterialInstanceDynamic::Create(DissolveMaterialInstance, this);
+		GetMesh()->SetMaterial(0, DynamicMatInst);
+		StartDissolveTimeline(DynamicMatInst);
+	}
+	if (IsValid(WeaponDissolveMaterialInstance))
+	{
+		UMaterialInstanceDynamic* DynamicMatInst = UMaterialInstanceDynamic::Create(WeaponDissolveMaterialInstance, this);
+		Weapon->SetMaterial(0, DynamicMatInst);
+		StartWeaponDissolveTimeline(DynamicMatInst);
+	}
 }
 
 UAbilitySystemComponent* AAuraCharacterBase::GetAbilitySystemComponent() const
